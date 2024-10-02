0c9dbb3e5951cc529944944718a78fc5d6104969
RonaldoMorales <ronaldo05032002@gmail.com>
2024-10-02 20:07:18 -0300

    feat: Interfaz creada

--- obj/Debug/net8.0/Catedra1.AssemblyInfo.cs
+++ obj/Debug/net8.0/Catedra1.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Catedra1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+99dbbdcc4486a6092ba53f64253fa87ebfd43912")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+43fcabfb4198c9efb4e92fee806400f9d0122630")]
 [assembly: System.Reflection.AssemblyProductAttribute("Catedra1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Catedra1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
---
+++ src/Interfaces/IUserRepository.cs
@@ -0,0 +1,8 @@
+using System;
+
+namespace Catedra1.src.Interfaces;
+
+public interface IUserRepository
+{
+
+}
